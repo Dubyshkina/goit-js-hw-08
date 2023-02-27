f00fbd40949801a139e98e229079410195af1431
Dubyshkina <ek.kalyazina@gmail.com>
2023-02-27 18:13:10 +0200

    did fist task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const makeGalleryMarkup = (galleryItem) => {
+  const { preview, original, description } = galleryItem;
+  return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src=${preview} title=${description}/>
+    </a>`;
+};
+const gallery = document.querySelector(".gallery");
+const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
+
+gallery.insertAdjacentHTML("afterbegin", makeGallery);
+console.log(makeGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    console.log(data);
+};
+
+player.on('timeupdate', onPlay);
+
 
