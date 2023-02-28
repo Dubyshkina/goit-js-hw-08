7c44a7e90a21ff47cb976bf5710388527b83da03
Dubyshkina <ek.kalyazina@gmail.com>
2023-02-28 19:16:47 +0200

    did 8 hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,14 @@
-import Player from "@vimeo/player";
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+let time = localStorage.getItem('videoplayer-current-time');
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-    console.log(data);
-};
+const onPlay = data =>
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-
+player.setCurrentTime(time || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+const userForm = document.querySelector('.feedback-form');
 
+const information = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+  email: '',
+  message: '',
+};
+
+userForm.email.value = information.email;
+userForm.message.value = information.message;
+
+const onInput = event => {
+  information[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(information));
+};
+userForm.addEventListener('input', throttle(onInput, 500));
+
+userForm.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+  console.log(information);
+  information.email = '';
+  information.message = '';
+});
